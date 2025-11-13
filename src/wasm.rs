5ace50a96b89afd25d4e623a9a71909e0631e2f5
Patrick José Pereira <patrickelectric@gmail.com>
2025-11-13 17:40:34 -0300

    Update wasm code to have idl generation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,8 +1,9 @@
 #![cfg(target_arch = "wasm32")]
 
-use wasm_bindgen::prelude::*;
+use js_sys::{Object, Reflect};
+use wasm_bindgen::{JsValue, prelude::*};
 
-use crate::parse_idl;
+use crate::{generator::generate_idl, parse_idl};
 
 #[wasm_bindgen(start)]
 pub fn wasm_start() {
@@ -17,4 +18,32 @@ pub fn parse_idl_wasm(input: &str) -> String {
         Ok(defs) => format!("{:#?}", defs),
         Err(err) => err,
     }
+}
+
+/// Parse and reserialize the given IDL, returning a JS object with both results.
+#[wasm_bindgen]
+pub fn analyze_idl_wasm(input: &str) -> JsValue {
+    let result = Object::new();
+    match parse_idl(input) {
+        Ok(defs) => {
+            let ast = format!("{:#?}", defs);
+            let generated = generate_idl(&defs);
+            Reflect::set(&result, &"ast".into(), &JsValue::from_str(&ast)).unwrap();
+            Reflect::set(&result, &"generated".into(), &JsValue::from_str(&generated)).unwrap();
+            Reflect::set(&result, &"error".into(), &JsValue::UNDEFINED).unwrap();
+        }
+        Err(err) => {
+            let err_str = JsValue::from_str(&err);
+            Reflect::set(&result, &"ast".into(), &err_str).unwrap();
+            let generated_msg = format!("Cannot generate IDL:\n{err}");
+            Reflect::set(
+                &result,
+                &"generated".into(),
+                &JsValue::from_str(&generated_msg),
+            )
+            .unwrap();
+            Reflect::set(&result, &"error".into(), &JsValue::from_str(&err)).unwrap();
+        }
+    }
+    result.into()
 }
