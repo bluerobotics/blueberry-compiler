6a6eff6a875106dba3d133aef3ee34a502959b3d
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-11-13 11:39:16 -0300

    ast/grammar: Add basic annotations apply
    * Allow for simple annotation process

--- src/ast.rs
+++ src/ast.rs
@@ -1,31 +1,61 @@
-/// Generic wrapper for AST nodes that can carry comments
+/// Generic wrapper for AST nodes that can carry comments and annotations
 #[derive(Debug, Clone, PartialEq)]
 pub struct Commented<T> {
     pub comments: Vec<String>,
+    pub annotations: Vec<Annotation>,
     pub node: T,
 }
 
 impl<T> Commented<T> {
     pub fn new(node: T, comments: Vec<String>) -> Self {
-        Commented { comments, node }
+        Commented {
+            comments,
+            annotations: Vec::new(),
+            node,
+        }
+    }
+
+    pub fn with_annotations(node: T, comments: Vec<String>, annotations: Vec<Annotation>) -> Self {
+        Commented {
+            comments,
+            annotations,
+            node,
+        }
     }
 }
 
 /// Trait for uniform access to comments
 pub trait HasComments {
-    fn comments(&self) -> &[String];
-    fn comments_mut(&mut self) -> &mut Vec<String>;
+  fn comments(&self) -> &[String];
+  fn comments_mut(&mut self) -> &mut Vec<String>;
 }
 
 impl<T> HasComments for Commented<T> {
-    fn comments(&self) -> &[String] {
-        &self.comments
-    }
+  fn comments(&self) -> &[String] {
+      &self.comments
+  }
 
-    fn comments_mut(&mut self) -> &mut Vec<String> {
-        &mut self.comments
-    }
+  fn comments_mut(&mut self) -> &mut Vec<String> {
+      &mut self.comments
+  }
 }
+
+/// Trait for uniform access to annotations
+pub trait HasAnnotations {
+  fn annotations(&self) -> &[Annotation];
+  fn annotations_mut(&mut self) -> &mut Vec<Annotation>;
+}
+
+impl<T> HasAnnotations for Commented<T> {
+  fn annotations(&self) -> &[Annotation] {
+      &self.annotations
+  }
+
+  fn annotations_mut(&mut self) -> &mut Vec<Annotation> {
+      &mut self.annotations
+  }
+}
+
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Definition {
@@ -81,6 +111,27 @@ pub enum ConstValue {
     Boolean(bool),
     Char(char),
     ScopedName(Vec<String>),
+}
+
+/// Applied annotation (e.g. @foo::bar(a = 1, b = "x"))
+#[derive(Debug, Clone, PartialEq)]
+pub struct Annotation {
+    /// The annotation name as a scoped identifier
+    pub name: Vec<String>,
+    /// Parameters to the annotation
+    pub params: Vec<AnnotationParam>,
+}
+
+/// Parameters to an applied annotation.
+///
+/// For now, only the "named" form is supported:
+///   @MyAnn(foo = 1, bar = TRUE)
+/// and the empty form:
+///   @MyAnn
+#[derive(Debug, Clone, PartialEq)]
+pub enum AnnotationParam {
+    Named { name: String, value: ConstValue },
+    // Positional(ConstValue),  // Can be added later if you want the shortened form
 }
 
 #[derive(Debug, Clone, PartialEq)]
